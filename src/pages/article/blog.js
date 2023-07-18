39daea1a5df74ee90ec043e89c9ae52a6377e065
Arman-Kabir <armankabir8@gmail.com>
2023-07-19 01:33:09 +0600

    nested routing

---
+++ src/pages/article/blog.js
@@ -0,0 +1,10 @@
+
+const BlogPage = () => {
+  return (
+    <div>
+        <h1>Welcome to PH blog</h1>
+    </div>
+  )
+}
+
+export default BlogPage
---
+++ src/pages/article/posts/post.js
@@ -0,0 +1,10 @@
+
+const PostPage = () => {
+  return (
+    <div>
+        <h1>This is nested post page</h1>
+    </div>
+  )
+}
+
+export default PostPage
